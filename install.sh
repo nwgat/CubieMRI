966b00f9b475d29bb47788c8f284f3c44138ad22
Nwgat <github@nwgat.net>
2014-05-11 04:29:11 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 # Nwgat.Net
 # http://github.com/nwgat
+# WORK IN PROGRESS / NOT WORKING
 
 JMRI_URL=$(curl -s http://jmri.org/releaselist -o - | tr '\n' ' ' | cut -d ":" -f 5,6 | cut -d " " -f 2 | cut -d '"' -f 2)
 JMRI_PACKAGE_NAME=$(curl -s http://jmri.org/releaselist -o - | tr '\n' ' ' | cut -d ":" -f 6 | cut -d "/" -f 8)
@@ -13,4 +14,9 @@ echo "" >>
 # jmri 
 wget -O $JMRI_PACKAGE_NAME "$JMRI_URL"
 tar zxvf $JMRI_PACKAGE_NAME
+
+apt-get install oracle-java7-installer librxr
+
+cp /usr/lib/jni/librxtxSerial.so $JAVA_HOME/jre/lib/i386/
+
 
