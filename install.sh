ac24a2743ca73b597db7b6f48a9cd4dff8953473
nwgat <github@nwgat.net>
2015-02-06 23:53:01 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-# Nwgat.Net
+# nwgat.ninja
 # http://github.com/nwgat
-# for Cubieez Linux ARM Distribution
+# all-in-one should work on both raspberry and cubie/banana
 # WORK IN PROGRESS / NOT WORKING
 
 JMRI_URL=$(curl -s http://jmri.org/releaselist -o - | tr '\n' ' ' | cut -d ":" -f 5,6 | cut -d " " -f 2 | cut -d '"' -f 2)
@@ -9,7 +9,11 @@ WORKING_DIR=$(pwd)
 ip=$(hostname -I)
 
 # install java
-apt-get install oracle-java7-installer librxtx
+echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee -a /etc/apt/sources.list
+echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee -a /etc/apt/sources.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
+apt-get update
+apt-get install oracle-java7-installer librxtx-java -y
 
 # install jmri 
 wget -O $JMRI_PACKAGE_NAME "$JMRI_URL"
@@ -17,10 +21,10 @@ tar zxvf $JMRI_PACKAGE_NAME
 ln -l /usr/lib/jvm/java-7-oracle/jre/lib/librxtxSerial.so /home/jmri/JMRI/lib/librxtxSerial.so
 
 # setting it up
-echo "su cubie -c "screen -dmS -X /home/jmri/JMRI/JmriFaceless"" >>
-
 
 # details
 echo "Details"
-echo "VNC: $ip:5900"
+echo "Supervisord WebUI:" 
+echo "http://$ip:9001"
+echo "config files on SFTP: $ip:22"
 echo "all done"
