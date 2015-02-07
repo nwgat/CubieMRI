5122987cff6976cdccf29a803b8e71480de492f6
nwgat <github@nwgat.net>
2015-02-07 14:29:10 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,6 @@
 # http://github.com/nwgat
 # all-in-one should work on both raspberry and cubie/banana
 # WORK IN PROGRESS / NOT WORKING
-
-JMRI_URL=$(curl -s http://jmri.org/releaselist -o - | tr '\n' ' ' | cut -d ":" -f 5,6 | cut -d " " -f 2 | cut -d '"' -f 2)
-JMRI_PACKAGE_NAME=$(curl -s http://jmri.org/releaselist -o - | tr '\n' ' ' | cut -d ":" -f 6 | cut -d "/" -f 8)
-WORKING_DIR=$(pwd)
 ip=$(hostname -I)
 
 # install java
@@ -13,18 +9,27 @@ echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee -
 echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee -a /etc/apt/sources.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
 apt-get update
-apt-get install oracle-java7-installer librxtx-java -y
+apt-get install oracle-java7-installer librxtx-java supervisor -y
 
-# install jmri 
-wget -O $JMRI_PACKAGE_NAME "$JMRI_URL"
-tar zxvf $JMRI_PACKAGE_NAME
-ln -l /usr/lib/jvm/java-7-oracle/jre/lib/librxtxSerial.so /home/jmri/JMRI/lib/librxtxSerial.so
+echo "[program:jmri]
+user=cubie
+directory=/home/cubie
+priority=2
+command=/home/cubie/JMRI/JmriFaceless
+redirect_stderr=true
+startsecs=5" > /etc/supervisor/conf.d/jmri.conf
 
-# setting it up
+echo "[inet_http_server] 
+port=*:9001" >> /etc/supervisor/supervisord.conf
+
+# install jmri
+cd $HOME && curl -L http://sourceforge.net/projects/jmri/files/production%20files/JMRI.3.10.1-r28327.tgz > /home/cubie/JMRI.tgz
+cd /home/cubie/ && tar xvf JMRI.tgz
+chown cubie:cubie -R /home/cubie/
+ln -s /usr/lib/jvm/java-7-oracle/jre/lib/librxtxSerial.so $HOME/JMRI/lib/librxtxSerial.so
 
 # details
-echo "Details"
-echo "Supervisord WebUI:" 
+echo "<<< Service Control WebUI >>>"
 echo "http://$ip:9001"
 echo "config files on SFTP: $ip:22"
 echo "all done"
