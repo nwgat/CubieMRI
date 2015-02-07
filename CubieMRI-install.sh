8dae8dc1901cbbc0af80743afadc8489ef35933c
nwgat <github@nwgat.net>
2015-02-07 14:36:27 +0100

    Update and rename install.sh to CubieMRI-install.sh

--- CubieMRI-install.sh
+++ CubieMRI-install.sh
@@ -4,6 +4,9 @@
 # WORK IN PROGRESS / NOT WORKING
 ip=$(hostname -I)
 
+what user to install jmri to? (cubie etc)
+read usr
+
 # install java
 echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee -a /etc/apt/sources.list
 echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee -a /etc/apt/sources.list
@@ -12,10 +15,10 @@ apt-get update
 apt-get install oracle-java7-installer librxtx-java supervisor -y
 
 echo "[program:jmri]
-user=cubie
-directory=/home/cubie
+user=$usr
+directory=/home/$usr
 priority=2
-command=/home/cubie/JMRI/JmriFaceless
+command=/home/$usr/JMRI/JmriFaceless
 redirect_stderr=true
 startsecs=5" > /etc/supervisor/conf.d/jmri.conf
 
@@ -23,13 +26,13 @@ echo "[inet_http_server]
 port=*:80" >> /etc/supervisor/supervisord.conf
 
 # install jmri
-cd $HOME && curl -L http://sourceforge.net/projects/jmri/files/production%20files/JMRI.3.10.1-r28327.tgz > /home/cubie/JMRI.tgz
-cd /home/cubie/ && tar xvf JMRI.tgz
-chown cubie:cubie -R /home/cubie/
+cd $HOME && curl -L http://sourceforge.net/projects/jmri/files/production%20files/JMRI.3.10.1-r28327.tgz > /home/$usr/JMRI.tgz
+cd /home/$usr/ && tar xvf JMRI.tgz
+chown $usr:$usr -R /home/$usr/
 ln -s /usr/lib/jvm/java-7-oracle/jre/lib/librxtxSerial.so $HOME/JMRI/lib/librxtxSerial.so
 
 # details
 echo "<<< Service Control WebUI >>>"
 echo "http://$ip"
 echo "config files on SFTP: $ip:22"
-echo "all done"
+echo "path is /home/$usr/.jmri"
